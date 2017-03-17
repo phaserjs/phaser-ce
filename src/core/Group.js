446f069397fb219da5108fce484dda051610cb82
Lightning3105 <james.waters@talktalk.net>
2017-03-17 11:52:27 +0000

    Fixed group updating if a sprite has been destroyed
    If a sprite is destroyed during another sprite's update function, the game will no longer crash.

--- src/core/Group.js
+++ src/core/Group.js
@@ -1648,6 +1648,7 @@ Phaser.Group.prototype.update = function () {
 
     while (i--)
     {
+        if (i >= this.children.length){i = this.children.length - 1}
         this.children[i].update();
     }
 
