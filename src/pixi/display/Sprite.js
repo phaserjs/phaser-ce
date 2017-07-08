cc8d0b4634f3cabad8672b7996113eed77fc6a2b
samme <samme@users.noreply.github.com>
2017-07-08 09:57:10 -0700

    PIXI.Sprite#anchor docs

--- src/pixi/display/Sprite.js
+++ src/pixi/display/Sprite.js
@@ -16,9 +16,11 @@ PIXI.Sprite = function (texture) {
 
     /**
      * The anchor sets the origin point of the texture.
-     * The default is 0,0 this means the texture's origin is the top left
-     * Setting than anchor to 0.5,0.5 means the textures origin is centered
-     * Setting the anchor to 1,1 would mean the textures origin points will be the bottom right corner
+     * The default (0, 0) is the top left.
+     * (0.5, 0.5) is the center.
+     * (1, 1) is the bottom right.
+     *
+     * You can modify the default values in PIXI.Sprite.defaultAnchor.
      *
      * @property anchor
      * @type Point
@@ -119,11 +121,9 @@ PIXI.Sprite = function (texture) {
 };
 
 /**
- * @property {object} PIXI.Sprite.defaultAnchor - A Point-like object.
- * @property {number} PIXI.Sprite.defaultAnchor.x
- * @default 0
- * @property {number} PIXI.Sprite.defaultAnchor.y
- * @default 0
+ * @property PIXI.Sprite.defaultAnchor - A Point-like object.
+ * @type {{x: number, y: number}}
+ * @default
  */
 PIXI.Sprite.defaultAnchor = {x: 0, y: 0};
 
