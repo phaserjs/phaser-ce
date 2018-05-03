f7b417df3b9eee40310a5e276ce189368c893d7d
samme <samme@users.noreply.github.com>
2018-05-03 15:36:07 -0700

    Bump version to 2.10.4

--- src/Phaser.js
+++ src/Phaser.js
@@ -14,7 +14,7 @@ var Phaser = Phaser || {    // jshint ignore:line
     * @constant Phaser.VERSION
     * @type {string}
     */
-    VERSION: '2.10.3',
+    VERSION: '2.10.4',
 
     /**
     * An array of Phaser game instances.
