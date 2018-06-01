1eeb9af7d3d788a46565a102ac1cc2126d8a87f5
samme <samme@users.noreply.github.com>
2018-05-31 21:09:34 -0700

    Correct Rectangle.aabb description
    Fixes #545
    
    Thanks @Siri0n

--- src/geom/Rectangle.js
+++ src/geom/Rectangle.js
@@ -1165,11 +1165,10 @@ Phaser.Rectangle.union = function (a, b, output)
 /**
 * Calculates the Axis Aligned Bounding Box (or aabb) from an array of points.
 *
-* @method Phaser.Rectangle#aabb
+* @method Phaser.Rectangle.aabb
 * @param {Phaser.Point[]} points - The array of one or more points.
 * @param {Phaser.Rectangle} [out] - Optional Rectangle to store the value in, if not supplied a new Rectangle object will be created.
 * @return {Phaser.Rectangle} The new Rectangle object.
-* @static
 */
 Phaser.Rectangle.aabb = function (points, out)
 {
