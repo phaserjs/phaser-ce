4a4185633693cbe24625978ad4945fc1b0ee51c1
samme <samme@users.noreply.github.com>
2018-06-26 10:56:42 -0700

    Fix 'illegal operation on a directory, copyfile' error

--- resources/docstrap-master/template/publish.js
+++ resources/docstrap-master/template/publish.js
@@ -588,7 +588,7 @@ exports.publish = function (taffyData, opts, tutorials) {
   staticFiles.forEach(function (fileName) {
     var toDir = fs.toDir(fileName.replace(fromDir, outdir));
     fs.mkPath(toDir);
-    fs.copyFileSync(fileName, toDir);
+    fs.copyFileSync(fileName, path.join(toDir, path.basename(fileName)));
   });
 
   // copy user-specified static files to outdir
@@ -611,7 +611,7 @@ exports.publish = function (taffyData, opts, tutorials) {
         var sourcePath = fs.toDir(filePath);
         var toDir = fs.toDir(fileName.replace(sourcePath, outdir));
         fs.mkPath(toDir);
-        fs.copyFileSync(fileName, toDir);
+        fs.copyFileSync(fileName, path.join(toDir, path.basename(fileName)));
       });
     });
   }
