a824ab9785eafd47c7425db568778b911f1aadeb
samme <samme@users.noreply.github.com>
2019-02-20 17:48:06 -0800

    Remove unnecessary conditions

--- src/tween/Easing.js
+++ src/tween/Easing.js
@@ -429,8 +429,7 @@ Phaser.Easing = {
                 p = 0.4;
             if (k === 0) { return 0; }
             if (k === 1) { return 1; }
-            if (!a || a < 1) { a = 1; s = p / 4; }
-            else { s = p * Math.asin(1 / a) / (2 * Math.PI); }
+            s = p * Math.asin(1 / a) / (2 * Math.PI);
             return - (a * Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p));
 
         },
@@ -450,8 +449,7 @@ Phaser.Easing = {
                 p = 0.4;
             if (k === 0) { return 0; }
             if (k === 1) { return 1; }
-            if (!a || a < 1) { a = 1; s = p / 4; }
-            else { s = p * Math.asin(1 / a) / (2 * Math.PI); }
+            s = p * Math.asin(1 / a) / (2 * Math.PI);
             return (a * Math.pow(2, - 10 * k) * Math.sin((k - s) * (2 * Math.PI) / p) + 1);
 
         },
@@ -471,8 +469,7 @@ Phaser.Easing = {
                 p = 0.4;
             if (k === 0) { return 0; }
             if (k === 1) { return 1; }
-            if (!a || a < 1) { a = 1; s = p / 4; }
-            else { s = p * Math.asin(1 / a) / (2 * Math.PI); }
+            s = p * Math.asin(1 / a) / (2 * Math.PI);
             if ((k *= 2) < 1) { return - 0.5 * (a * Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p)); }
             return a * Math.pow(2, -10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p) * 0.5 + 1;
 
