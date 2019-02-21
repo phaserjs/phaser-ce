af50dc29c82804eee2b6e446adf7473a8c9b7d62
Jamie Allen <jamie.allen@prodigygame.com>
2019-02-21 17:04:59 -0500

    Revert comments formatting.

--- typescript/phaser.comments.d.ts
+++ typescript/phaser.comments.d.ts
@@ -7662,7 +7662,6 @@ declare module Phaser {
         alignH?: boolean;
         alignV?: boolean;
         antialias?: boolean;
-        batchRender?: boolean;
         backgroundColor?: number | string;
         canvas?: HTMLCanvasElement;
         canvasId?: string;
