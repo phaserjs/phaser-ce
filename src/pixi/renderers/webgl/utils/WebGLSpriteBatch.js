8fa6cb025cc922a8f7432a1075bb2686d751c8cc
samme <samme@users.noreply.github.com>
2021-10-24 14:39:36 -0700

    Revert "Merge pull request #708 from noocsharp/master"
    This reverts commit 3dbfafacba3d35bde8445289a04c6f26fe33948d, reversing
    changes made to 232f8571b5334784c8595bb276d9ba0512a73a0d.

--- src/pixi/renderers/webgl/utils/WebGLSpriteBatch.js
+++ src/pixi/renderers/webgl/utils/WebGLSpriteBatch.js
@@ -637,10 +637,7 @@ PIXI.WebGLSpriteBatch.prototype.flush = function ()
         gl.vertexAttribPointer(shader.colorAttribute, 4, gl.UNSIGNED_BYTE, true, stride, 16);
 
         // Texture index
-        if (PIXI._enableMultiTextureToggle)
-        {
-            gl.vertexAttribPointer(shader.aTextureIndex, 1, gl.FLOAT, false, stride, 20);
-        }
+        gl.vertexAttribPointer(shader.aTextureIndex, 1, gl.FLOAT, false, stride, 20);
     }
 
     // upload the verts to the buffer
